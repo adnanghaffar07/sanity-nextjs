8678e532458da7b81cdf7f9dd162fdd53637c5a5
umaid-ca <umaid@codeautomation.ai>
2024-03-26 00:31:01 +0500

    case-study sub page error fixed

--- app/(site)/case-study/[singlecase]/page.tsx
+++ app/(site)/case-study/[singlecase]/page.tsx
@@ -34,41 +34,41 @@ const page = () => {
               />
             </div>
 
-        <div>
-          <div>
-            <h3 className="text-sm leading-5 sm:text-xl font-normal mb-5 max-w-[1020px] text-justify sm:leading-8">
-              The brief
-            </h3>
-            <p>
-              Bubblehouse is a player in a new market, coming up with
-              tailor-made e-commerce solutions to boost clients&apos; brand loyalty.
-              Comprehensive VIP customer platform which incorporates tiers,
-              collectibles, referrals and other entertaining elements to
-              encourage customers and build loyalty over the years and long term
-              value (LTV). The end objective of Bubblehouse project was to
-              introduce a powerful e-commerce loyalty solution that makes it
-              possible for both individual online stores and subscriptions doing
-              business.
-            </p>
-          </div>
-          <div>
             <div>
-              <p>Industry</p>
-              <p>E-Commerce</p>
+              <div>
+                <h3 className="text-sm leading-5 sm:text-xl font-normal mb-5 max-w-[1020px] text-justify sm:leading-8">
+                  The brief
+                </h3>
+                <p>
+                  Bubblehouse is a player in a new market, coming up with
+                  tailor-made e-commerce solutions to boost clients&apos; brand
+                  loyalty. Comprehensive VIP customer platform which
+                  incorporates tiers, collectibles, referrals and other
+                  entertaining elements to encourage customers and build loyalty
+                  over the years and long term value (LTV). The end objective of
+                  Bubblehouse project was to introduce a powerful e-commerce
+                  loyalty solution that makes it possible for both individual
+                  online stores and subscriptions doing business.
+                </p>
+              </div>
+              <div>
+                <div>
+                  <p>Industry</p>
+                  <p>E-Commerce</p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <img
+                alt="bubblehouse project"
+                loading="lazy"
+                src="/casesubpage-bubblehouse2.png"
+                // layout="fill"
+                className="size-full mb-6 sm:mb-12 shadow-blogImage max-w-[1100px] mx-auto"
+              />
             </div>
           </div>
-        </div>
-
-        <div>
-          <img
-            alt="bubblehouse project"
-            loading="lazy"
-            src="/casesubpage-bubblehouse2.png"
-            // layout="fill"
-            className="size-full mb-6 sm:mb-12 shadow-blogImage max-w-[1100px] mx-auto"
-          />
-        </div>
-      </div>
 
           <div>
             <img
@@ -77,160 +77,167 @@ const page = () => {
               className="size-full mb-0 sm:mb-14 shadow-blogImage"
             />
 
-        <div className="max-w-[1020px] sm:leading-8 text-justify mb-9 sm:text-xl font-normal text-sm leading-5 ">
-          <h2 className=" xl:text-4xl lg:text-2xl text-xl font-semibold mb-5">
-            Project Scope:
-          </h2>
-          <p>
-            In our cooperation with Bubblehouse we provided backend development,
-            integrations setup, and manual testing. The two major goals were to
-            develop an amazing e-commerce solution that does not hinder
-            customers&apos; experience and assure the reliability of the application
-            by opting for manual and automated testing methods.
-          </p>
-          <h2 className=" xl:text-4xl lg:text-2xl text-xl font-semibold mb-5">
-            Technologies used:
-          </h2>
-          <ul>
-            <li>
-              <span>Shopify Development:</span> The heartbeat of the project
-              was, among other things, to produce & incorporate loyalty
-              attributes within Shopify e-shop platform.
-            </li>
-          </ul>
-          <p>
-            Consider the e-commerce industry. If mobile conversion rates are
-            poor, it would be better for the mobile user to do business by
-            addressing problems with product browsing rather than concentrating
-            on checkout.
-          </p>
-          <p>
-            For instance, improving the responsive site&rsquo;s search results,
-            adding product filters, deepening product descriptions, and speeding
-            up page load times (so that the entire catalogue is accessible).
-          </p>
-          <p>
-            While you browse, find out where your consumers are. How can the
-            experience be improved if the customer is at the store at home or
-            traveling on their bed? Businesses should use these inquiries to
-            describe the issue and their plans to address it.
-          </p>
+            <div className="max-w-[1020px] sm:leading-8 text-justify mb-9 sm:text-xl font-normal text-sm leading-5 ">
+              <h2 className=" xl:text-4xl lg:text-2xl text-xl font-semibold mb-5">
+                Project Scope:
+              </h2>
+              <p>
+                In our cooperation with Bubblehouse we provided backend
+                development, integrations setup, and manual testing. The two
+                major goals were to develop an amazing e-commerce solution that
+                does not hinder customers&apos; experience and assure the
+                reliability of the application by opting for manual and
+                automated testing methods.
+              </p>
+              <h2 className=" xl:text-4xl lg:text-2xl text-xl font-semibold mb-5">
+                Technologies used:
+              </h2>
+              <ul>
+                <li>
+                  <span>Shopify Development:</span> The heartbeat of the project
+                  was, among other things, to produce & incorporate loyalty
+                  attributes within Shopify e-shop platform.
+                </li>
+              </ul>
+              <p>
+                Consider the e-commerce industry. If mobile conversion rates are
+                poor, it would be better for the mobile user to do business by
+                addressing problems with product browsing rather than
+                concentrating on checkout.
+              </p>
+              <p>
+                For instance, improving the responsive site&rsquo;s search
+                results, adding product filters, deepening product descriptions,
+                and speeding up page load times (so that the entire catalogue is
+                accessible).
+              </p>
+              <p>
+                While you browse, find out where your consumers are. How can the
+                experience be improved if the customer is at the store at home
+                or traveling on their bed? Businesses should use these inquiries
+                to describe the issue and their plans to address it.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            1. Building a mobile strategy that works
-          </h3>
-          <p>
-            Mobile strategy is not based on conjecture. It consists of solutions
-            that are not only focused on features and functions but also on how
-            your clients use handheld and mobile devices in their daily lives.
-          </p>
-          <p>
-            Here&rsquo;s how to develop a flexible mobile plan to include in
-            your marketing effort to get the best results.
-          </p>
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            2. Design mesmerizing experiences
-          </h3>
-          <p>
-            Concentrate on offering a user-interaction platform that enables
-            people to engage with your brand in novel and engaging ways. Most
-            consumers expect their applications to launch in two seconds or
-            less, and 85% of users prefer apps over websites.
-          </p>
-          <p>
-            A mobile website with a distorted style and challenging navigation
-            may have disappointed you. Create your app to avoid these
-            annoyances.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                1. Building a mobile strategy that works
+              </h3>
+              <p>
+                Mobile strategy is not based on conjecture. It consists of
+                solutions that are not only focused on features and functions
+                but also on how your clients use handheld and mobile devices in
+                their daily lives.
+              </p>
+              <p>
+                Here&rsquo;s how to develop a flexible mobile plan to include in
+                your marketing effort to get the best results.
+              </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                2. Design mesmerizing experiences
+              </h3>
+              <p>
+                Concentrate on offering a user-interaction platform that enables
+                people to engage with your brand in novel and engaging ways.
+                Most consumers expect their applications to launch in two
+                seconds or less, and 85% of users prefer apps over websites.
+              </p>
+              <p>
+                A mobile website with a distorted style and challenging
+                navigation may have disappointed you. Create your app to avoid
+                these annoyances.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            3. Performance is the Master Key
-          </h3>
-          <p>
-            According to research, 57% of visitors leave your website if it
-            takes longer than 3 seconds to load. Yet you might be surprised that
-            about 80% of these visitors won&rsquo;t return.
-          </p>
-          <p>
-            This means you must concentrate on your website&rsquo;s performance
-            across all devices to keep your business afloat and flourish in the
-            expanding industry. Ensure your website/application runs as smoothly
-            on mobile devices as on desktops.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                3. Performance is the Master Key
+              </h3>
+              <p>
+                According to research, 57% of visitors leave your website if it
+                takes longer than 3 seconds to load. Yet you might be surprised
+                that about 80% of these visitors won&rsquo;t return.
+              </p>
+              <p>
+                This means you must concentrate on your website&rsquo;s
+                performance across all devices to keep your business afloat and
+                flourish in the expanding industry. Ensure your
+                website/application runs as smoothly on mobile devices as on
+                desktops.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            4. Better Voice assistance for easy search
-          </h3>
-          <p>
-            The ability to speak and express oneself is now the greatest gift
-            &rsquo;people on earth&rsquo; have. Tony Zhao, the founder and CEO
-            of the video chat business Agora.io stated in an interview that
-            in-app conversations could raise user interest and engagement to
-            some level. The same holds for your guests.
-          </p>
-          <p>
-            You can quickly implement video calling or a click-to-call feature
-            for audio calls on your application or website. Many click-to-call
-            Software Development Kits are available online for the same. These
-            kids must use a few coding lines and procedures to deliver their
-            needs.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                4. Better Voice assistance for easy search
+              </h3>
+              <p>
+                The ability to speak and express oneself is now the greatest
+                gift &rsquo;people on earth&rsquo; have. Tony Zhao, the founder
+                and CEO of the video chat business Agora.io stated in an
+                interview that in-app conversations could raise user interest
+                and engagement to some level. The same holds for your guests.
+              </p>
+              <p>
+                You can quickly implement video calling or a click-to-call
+                feature for audio calls on your application or website. Many
+                click-to-call Software Development Kits are available online for
+                the same. These kids must use a few coding lines and procedures
+                to deliver their needs.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            5. Scalable with real-time analytics
-          </h3>
-          <p>
-            The smartphone market is expected to expand by more than 10% in the
-            next four to five years. The same is true for app stores, which are
-            predicted to have double-digit download growth over the next few
-            years.
-          </p>
-          <p>
-            You must ensure that your strategy, execution, and output are
-            scalable to keep up with this expanding industry and establish a
-            location. For all of your data and performance requirements, you
-            have real-time analytics.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                5. Scalable with real-time analytics
+              </h3>
+              <p>
+                The smartphone market is expected to expand by more than 10% in
+                the next four to five years. The same is true for app stores,
+                which are predicted to have double-digit download growth over
+                the next few years.
+              </p>
+              <p>
+                You must ensure that your strategy, execution, and output are
+                scalable to keep up with this expanding industry and establish a
+                location. For all of your data and performance requirements, you
+                have real-time analytics.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            6. Monetization of mobile apps
-          </h3>
-          <p>
-            Think about providing a free version of your software and letting
-            customers choose whether or not they want to pay for a premium
-            edition with additional features and content. Consider the
-            enormously well-liked Angry Birds iPhone game.
-          </p>
-          <p>
-            Offer a free version while granting paid members access to harder
-            levels and other free add-ons as part of their continuous campaign.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                6. Monetization of mobile apps
+              </h3>
+              <p>
+                Think about providing a free version of your software and
+                letting customers choose whether or not they want to pay for a
+                premium edition with additional features and content. Consider
+                the enormously well-liked Angry Birds iPhone game.
+              </p>
+              <p>
+                Offer a free version while granting paid members access to
+                harder levels and other free add-ons as part of their continuous
+                campaign.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            7. Build a team
-          </h3>
-          <p>
-            Basic software can cost $10,000 to develop, while a graphics-heavy
-            app might cost millions. To create successful apps, you need a
-            strong team of developers. The beginning is the internal development
-            team.
-          </p>
-          <p>
-            Thanks to this, you will have the technical know-how to carry out
-            your strategy, and you won&rsquo;t need to negotiate a
-            feature&rsquo;s implementation with an outside contractor.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                7. Build a team
+              </h3>
+              <p>
+                Basic software can cost $10,000 to develop, while a
+                graphics-heavy app might cost millions. To create successful
+                apps, you need a strong team of developers. The beginning is the
+                internal development team.
+              </p>
+              <p>
+                Thanks to this, you will have the technical know-how to carry
+                out your strategy, and you won&rsquo;t need to negotiate a
+                feature&rsquo;s implementation with an outside contractor.
+              </p>
 
-          <h3 className="font-semibold text-base sm:text-2xl mt-5">
-            The Final Verdict
-          </h3>
-          <p>
-            The top seven mobile business strategies were those listed above.
-            The knowledge may be summarised by saying that you require a skilled
-            group of developers. Without that, you may also think about working
-            with a technical partner or consultant to prevent these minor
-            mistakes.
-          </p>
+              <h3 className="font-semibold text-base sm:text-2xl mt-5">
+                The Final Verdict
+              </h3>
+              <p>
+                The top seven mobile business strategies were those listed
+                above. The knowledge may be summarised by saying that you
+                require a skilled group of developers. Without that, you may
+                also think about working with a technical partner or consultant
+                to prevent these minor mistakes.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
