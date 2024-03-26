306aeff9fc6baf6389d142db1918d6e9f8479373
AyeshaMunir5365 <ayesha@codeautomation.dev>
2024-03-26 12:05:44 +0500

    form fixed

--- app/(site)/components/project-discussion-form.tsx
+++ app/(site)/components/project-discussion-form.tsx
@@ -1,32 +1,29 @@
 'use client'
 import React, { useState } from 'react';
 
+
 export default function ProjectDiscussionContainer() {
     const [formData, setFormData] = useState({
-        name: { value: '', placeholder: 'Your Name', pattern: '', errormessage: 'Your name is required' },
-        contactNumber: { value: '', placeholder: 'Your Contact Number', pattern: '', errormessage: 'Your contact number is required' },
-        email: { value: '', placeholder: 'Your Email', pattern: '', errormessage: 'Your email is required' },
-        lookingFor: { value: '', placeholder: 'What are you looking for?', pattern: '', errormessage: 'Your looking for information is required' },
-        message: { value: '', placeholder: 'Your Message', pattern: '', errormessage: 'Your message is required' },
+        name: { value: '', placeholder: 'Your Name', pattern: ' ', errormessage: 'Your name is required!', required:true },
+        contactNumber: { value: '', placeholder: 'Your Contact Number', pattern: ' ', errormessage: 'Your contact number is required!' },
+        email: { value: '', placeholder: 'Your Email', pattern: ' ', errormessage: 'Please Enter A valid Email!' },
+        lookingFor: { value: '', placeholder: 'What are you looking for?', pattern: ' ', errormessage: 'Your looking for information is required!' },
+        message: { value: '', placeholder: 'Your Message', pattern: ' ', errormessage: 'Your message is required!' },
     });
     const [isClicked, setIsClicked] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleChange = (e:any) => {
         const { name, value } = e.target;
-        setFormData((prevData: any) => ({ ...prevData, [name]: { ...prevData[name as keyof typeof formData], value } }));
+        setFormData((prevData:any) => ({ ...prevData, [name]: { ...prevData[name], value } }));
     };
 
     const handleClick = () => {
-        const isValidForm = Object.values(formData).every(field => field.value !== '' );
-    
-        if (isValidForm) {
-            setIsClicked(true);
-            setIsModalOpen(true);
-            console.log('Form Data:', formData);
-        }
+        setIsClicked(true);
+        setIsModalOpen(true);
+        console.log('Form Data:', formData);
     };
-    
+
     const handleCloseModal = () => {
         setIsModalOpen(false);
     };
@@ -40,6 +37,7 @@ export default function ProjectDiscussionContainer() {
                 {Object.entries(formData).map(([key, { value, placeholder, pattern, errormessage }]) => (
                     <div key={key} className="mt-7">
                         {key === 'message' ? (
+                              <>
                             <textarea
                                 className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black resize-none"
                                 placeholder={placeholder}
@@ -49,6 +47,10 @@ export default function ProjectDiscussionContainer() {
                                 value={value}
                                 onChange={handleChange}
                             />
+                            {isClicked && value === '' && (
+                                <p className="text-xs text-red-500 ml-2 mt-2">{errormessage}</p>
+                            )}
+                            </>
                         ) : (
                             <>
                                 <input
@@ -59,7 +61,7 @@ export default function ProjectDiscussionContainer() {
                                     value={value}
                                     onChange={handleChange}
                                 />
-                                {isClicked && value === '' && (
+                                {isClicked && value === '' &&  !value.match(pattern) &&(
                                     <p className="text-xs text-red-500 ml-2 mt-2">{errormessage}</p>
                                 )}
                             </>
@@ -73,7 +75,7 @@ export default function ProjectDiscussionContainer() {
                 >
                     Submit
                 </div>
-                {isModalOpen && (
+                {isModalOpen && Object.values(formData).every(field => field.value !== '')&& (
                     <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 flex items-center justify-center">
                         <div className="bg-white rounded-md p-8">
                             <h2 className="text-1xl font-bold mb-4">Submitted</h2>
