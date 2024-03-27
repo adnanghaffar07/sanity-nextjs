2faf0a7f521d878358bb9be6e3d76d4cea17164c
AyeshaMunir5365 <ayesha@codeautomation.dev>
2024-03-27 23:11:33 +0500

    errors fixed

--- app/(site)/components/project-discussion-form.tsx
+++ app/(site)/components/project-discussion-form.tsx
@@ -1,32 +1,20 @@
 'use client'
-
-import Link from "next/link";
-import { useFormik } from "formik"
-import { contactSchema } from "../../schemas/index"
+import { useFormik } from "formik";
+import { contactSchema } from "../../schemas/index";
 import { useForm } from '@formspree/react';
+
 const initialValues = {
     name: "",
     contact_number: "",
     looking: "",
     email: "",
     message: ""
-
-}
-import { useFormik } from "formik"
-import { contactSchema } from "../../schemas/index"
-import { useForm } from '@formspree/react';
-const initialValues = {
-    name: "",
-    contact_number: "",
-    looking: "",
-    email: "",
-    message: ""
-
-}
+};
 
 export default function ProjectDiscussionContainer() {
     const [state, formspreeHandleSubmit] = useForm("moqgkqrp");
-    const handleCombinedSubmit = async (event:any): Promise<void> => {
+
+    const handleCombinedSubmit = async (event:any) => {
         // Validate the form using Formik
         handleSubmit(event);
 
@@ -55,9 +43,7 @@ export default function ProjectDiscussionContainer() {
                 <div className="lg:text-4xl md:text-3xl text-l font-medium  text-center leading-[52px] max-md:max-w-full max-md:text-4xl">
                     Lets have a Project Discussion
                 </div>
-                <form action="https://formspree.io/f/moqgkqrp"
-                method="POST"
-                onSubmit={handleCombinedSubmit}>
+                <form action="https://formspree.io/f/moqgkqrp" method="POST" onSubmit={handleCombinedSubmit}>
                     <div className="mt-10">
                         <input
                             className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black"
@@ -67,8 +53,7 @@ export default function ProjectDiscussionContainer() {
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {errors.name && touched.name? 
-                        (<p className="form-error">{errors.name}</p>): null}
+                        {errors.name && touched.name && (<p className="form-error">{errors.name}</p>)}
                     </div>
                     <div className="mt-4">
                         <input
@@ -79,8 +64,7 @@ export default function ProjectDiscussionContainer() {
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {errors.contact_number && touched.contact_number? 
-                        (<p className="form-error">{errors.contact_number}</p>): null}
+                        {errors.contact_number && touched.contact_number && (<p className="form-error">{errors.contact_number}</p>)}
                     </div>
                     <div className="mt-4">
                         <input
@@ -91,8 +75,7 @@ export default function ProjectDiscussionContainer() {
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {errors.email && touched.email? 
-                        (<p className="form-error">{errors.email}</p>): null}
+                        {errors.email && touched.email && (<p className="form-error">{errors.email}</p>)}
                     </div>
                     <div className="mt-4">
                         <input
@@ -103,8 +86,7 @@ export default function ProjectDiscussionContainer() {
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {errors.looking && touched.looking? 
-                        (<p className="form-error">{errors.looking}</p>): null}
+                        {errors.looking && touched.looking && (<p className="form-error">{errors.looking}</p>)}
                     </div>
                     <div className="mt-4">
                         <textarea
@@ -117,89 +99,13 @@ export default function ProjectDiscussionContainer() {
                             rows={4}
                             cols={4}
                         />
-                        {errors.message && touched.message? 
-                        (<p className="form-error">{errors.message}</p>): null}
+                        {errors.message && touched.message && (<p className="form-error">{errors.message}</p>)}
                     </div>
-                </div>
-                <form action="https://formspree.io/f/moqgkqrp"
-                method="POST"
-                onSubmit={handleCombinedSubmit}>
-                    <div className="mt-10">
-                        <input
-                            className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black"
-                            placeholder={'Name'}
-                            name="name"
-                            value={values.name}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {errors.name && touched.name? 
-                        (<p className="form-error">{errors.name}</p>): null}
-                    </div>
-                    <div className="mt-4">
-                        <input
-                            className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black"
-                            placeholder={'Contact Number'}
-                            name="contact_number"
-                            value={values.contact_number}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {errors.contact_number && touched.contact_number? 
-                        (<p className="form-error">{errors.contact_number}</p>): null}
-                    </div>
-                    <div className="mt-4">
-                        <input
-                            className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black"
-                            placeholder={'Email'}
-                            name="email"
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {errors.email && touched.email? 
-                        (<p className="form-error">{errors.email}</p>): null}
-                    </div>
-                    <div className="mt-4">
-                        <input
-                            className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black"
-                            placeholder={'What are you looking for?'}
-                            name="looking"
-                            value={values.looking}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {errors.looking && touched.looking? 
-                        (<p className="form-error">{errors.looking}</p>): null}
-                    </div>
-                    <div className="mt-4">
-                        <textarea
-                            className="border-2 justify-center items-start px-7 py-3 whitespace-nowrap rounded-xl shadow-sm bg-zinc-100 max-md:px-5 w-full text-black text-sm placeholder-black resize-none"
-                            placeholder={'Your Message'}
-                            name="message"
-                            value={values.message}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            rows={4}
-                            cols={4}
-                        />
-                        {errors.message && touched.message? 
-                        (<p className="form-error">{errors.message}</p>): null}
-                    </div>
-
-
                     <button type="submit" className="self-center mt-6 shadow-md text-base xl:text-1xl lg:text-xl text-center text-black max-md:mt-10 bg-[#F7E022] flex w-full justify-center rounded-xl py-2 cursor-pointer hover:cursor-pointer">
                         Submit
                     </button>
-
                 </form>
-                    <button type="submit" className="self-center mt-6 shadow-md text-base xl:text-1xl lg:text-xl text-center text-black max-md:mt-10 bg-[#F7E022] flex w-full justify-center rounded-xl py-2 cursor-pointer hover:cursor-pointer">
-                        Submit
-                    </button>
-
-                </form>
-
             </div>
         </div>
     );
-};
+}
