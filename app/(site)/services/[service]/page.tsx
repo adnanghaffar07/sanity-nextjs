35209afa039b1250d7566e1a9456cd2e2c3ec189
umaid-ca <umaid@codeautomation.ai>
2024-04-02 19:57:12 +0500

    video code update

--- app/(site)/services/[service]/page.tsx
+++ app/(site)/services/[service]/page.tsx
@@ -34,9 +34,46 @@ const page = async ({ params }: { params: { service: String } }) => {
   //   client.config().dataset
   // }/${fileId}.${extension}`;
 
+  let videoUrl;
+  if (
+    data.bannervideo &&
+    data.bannervideo.asset &&
+    data.bannervideo.asset._ref
+  ) {
+    const ref = data.bannervideo.asset._ref;
+    const [, fileId, extension] = ref.match(/file-(.+)-(.+)/);
+    videoUrl = `https://cdn.sanity.io/files/${client.config().projectId}/${
+      client.config().dataset
+    }/${fileId}.${extension}`;
+    // console.log(videoUrl);
+  } else {
+    console.error("The bannervideo or its asset property is undefined.");
+  }
+
+  const videoBuiler = async () => {
+    const ref = await data.bannervideo.asset._ref;
+    const [, fileId, extension] = await ref.match(/file-(.+)-(.+)/);
+    // console.log("_________url video___________", fileId);
+
+    return `https://cdn.sanity.io/files/${client.config().projectId}/${
+      client.config().dataset
+    }/${fileId}.${extension}`;
+  };
+
+  console.log("_____first", await videoBuiler());
+
+  //   videoBuiler()
+  //     .then((url) => {
+  //       return url;
+  //     })
+  //     .then((dat) => {
+  //       return dat;
+  //     })
+  // );
+
   return (
     <>
-      {/* <section className="relative">
+      <section className="relative">
         <div className="bg-black w-full h-[380px] sm:h-[700px] opacity-65 absolute z-[1]"></div>
         <div className="w-full h-[380px] sm:h-[700px] relative z-0">
           <video
@@ -59,7 +96,7 @@ const page = async ({ params }: { params: { service: String } }) => {
             {data.serviceDesceBaner}
           </p>
         </div>
-      </section> */}
+      </section>
       <HeroSectionComponent
         title={`"${data.serviceTitleBaner}"`}
         content={data.serviceDesceBaner}
