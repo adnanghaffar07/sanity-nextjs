23a0313f7432f7e547aa0bf9f708d54fbe62152e
umaid-ca <umaid@codeautomation.ai>
2024-04-22 23:47:19 +0500

    sanity data model for FAQs created and integrated

---
+++ sanity/schemas/faq.ts
@@ -0,0 +1,19 @@
+const faq = {
+  name: "faq",
+  title: "FAQs",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "string",
+    },
+  ],
+};
+
+export default faq;
