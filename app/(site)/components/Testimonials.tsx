f41eab68b874329331550b7725883ca16b5ca89e
umaid-ca <umaid@codeautomation.ai>
2024-05-06 23:11:18 +0500

    updated testimonial slider

--- app/(site)/components/Testimonials.tsx
+++ app/(site)/components/Testimonials.tsx
@@ -60,31 +60,63 @@ const Testimonials = () => {
                   <p className="self-end text-xl sm:text-3xl font-medium text-white">
                     Clients Speaking
                   </p>
-                  <div className="flex z-10 flex-col py-7 px-5 w-full bg-white rounded-2xl shadow-sm backdrop-blur-[6.5px] max-md:pr-5 absolute md:-bottom-20 md:top-auto top-20 -bottom-auto md:-left-20 -left-4 md:h-[270px]">
-                    <div className="text-[12px] sm:text-sm leading-5 text-justify text-black">
-                      {testimonial.content}
-                    </div>
-                    <div className="flex gap-3 justify-between mt-4 max-md:mr-1">
-                      <div className="flex justify-center items-center px-1 rounded-xl aspect-square w-[68px] h-[68px] bg-[#00000033]">
-                        <img
-                          loading="lazy"
-                          src={urlForImage(testimonial.image.asset)}
-                          alt={testimonial.image.alt}
-                          className="aspect-square rounded-full w-[62px] h-[62px]"
-                        />
+                  {testimonial.link ? (
+                    <a
+                      href={testimonial.link}
+                      target="_blank"
+                      className="flex z-10 flex-col py-7 px-5 w-full bg-white rounded-2xl shadow-sm backdrop-blur-[6.5px] max-md:pr-5 absolute md:-bottom-20 md:top-auto top-20 -bottom-auto md:-left-20 -left-4 md:h-[270px]"
+                    >
+                      <div className="text-[12px] sm:text-sm leading-5 text-justify text-black">
+                        {testimonial.content}
                       </div>
-                      <div className="flex flex-col flex-1 self-start mt-3 text-sky-950">
-                        {testimonial.name && (
-                          <div className="text-xl sm:text-2xl lg:text-3xl font-medium">
-                            {testimonial.name}
+                      <div className="flex gap-3 justify-between mt-4 max-md:mr-1">
+                        <div className="flex justify-center items-center px-1 rounded-xl aspect-square w-[68px] h-[68px] bg-[#00000033]">
+                          <img
+                            loading="lazy"
+                            src={urlForImage(testimonial.image.asset)}
+                            alt={testimonial.image.alt}
+                            className="aspect-square rounded-full w-[62px] h-[62px]"
+                          />
+                        </div>
+                        <div className="flex flex-col flex-1 self-start mt-3 text-sky-950">
+                          {testimonial.name && (
+                            <div className="text-xl sm:text-2xl lg:text-3xl font-medium">
+                              {testimonial.name}
+                            </div>
+                          )}
+                          <div className="text-sm sm:text-base lg:text-lg font-light leading-6">
+                            {testimonial.designation}
                           </div>
-                        )}
-                        <div className="text-sm sm:text-base lg:text-lg font-light leading-6">
-                          {testimonial.designation}
+                        </div>
+                      </div>
+                    </a>
+                  ) : (
+                    <div className="flex z-10 flex-col py-7 px-5 w-full bg-white rounded-2xl shadow-sm backdrop-blur-[6.5px] max-md:pr-5 absolute md:-bottom-20 md:top-auto top-20 -bottom-auto md:-left-20 -left-4 md:h-[270px]">
+                      <div className="text-[12px] sm:text-sm leading-5 text-justify text-black">
+                        {testimonial.content}
+                      </div>
+                      <div className="flex gap-3 justify-between mt-4 max-md:mr-1">
+                        <div className="flex justify-center items-center px-1 rounded-xl aspect-square w-[68px] h-[68px] bg-[#00000033]">
+                          <img
+                            loading="lazy"
+                            src={urlForImage(testimonial.image.asset)}
+                            alt={testimonial.image.alt}
+                            className="aspect-square rounded-full w-[62px] h-[62px]"
+                          />
+                        </div>
+                        <div className="flex flex-col flex-1 self-start mt-3 text-sky-950">
+                          {testimonial.name && (
+                            <div className="text-xl sm:text-2xl lg:text-3xl font-medium">
+                              {testimonial.name}
+                            </div>
+                          )}
+                          <div className="text-sm sm:text-base lg:text-lg font-light leading-6">
+                            {testimonial.designation}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             ))}
