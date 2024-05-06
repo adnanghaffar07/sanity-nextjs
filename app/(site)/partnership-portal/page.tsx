0553cd70b479cfe730e6d915c77fef332ed8d848
umaid-ca <umaid@codeautomation.ai>
2024-05-06 16:39:17 +0500

    partnership-portal (continue)

---
+++ app/(site)/partnership-portal/page.tsx
@@ -0,0 +1,273 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section className="flex flex-col pt-20 pr-12 bg-white max-md:pr-5">
+        <main className="mt-24 max-md:mt-10 max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <article className="flex flex-col w-[62%] max-md:ml-0 max-md:w-full">
+              <section className="flex flex-col grow max-md:mt-10 max-md:max-w-full">
+                <div className="flex flex-col pr-9 pl-20 max-md:px-5 max-md:max-w-full">
+                  <h1 className="text-5xl text-black leading-[58px] max-md:max-w-full max-md:text-4xl max-md:leading-[52px]">
+                    Welcome to our Partnership Portal
+                  </h1>
+                  <p className="mt-11 text-base leading-6 text-slate-600 max-md:mt-10 max-md:max-w-full">
+                    At CodeAutomation, a leader in Web and Mobile, Automation,
+                    QA technology solutions, we are excited to introduce three
+                    dynamic partnership opportunities designed to grow your
+                    business and enhance your earnings. Whether you are looking
+                    to earn by referring clients or take a more hands-on
+                    approach in reselling our services, we have the right
+                    program for you. Explore our Affiliate, Reseller, and White
+                    Label Partner channels and find out how you can start
+                    benefiting today!
+                    <br />
+                  </p>
+                </div>
+                <div className="z-10 mt-0 max-md:max-w-full">
+                  <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+                    <div className="flex flex-col w-[83%] max-md:ml-0 max-md:w-full">
+                      <div className="flex flex-col grow items-end py-20 pl-20 text-lg leading-7 rounded-full border-solid border-[61px] border-yellow-400 border-opacity-40 text-slate-600 max-md:mt-10 max-md:max-w-full">
+                        <p className="mt-3.5 text-base  leading-6 max-md:max-w-full">
+                          Expand Your Business with CodeAutomation
+                        </p>
+                        <div className="flex gap-5 self-start mt-12 ml-5 font-bold text-black max-md:mt-10 max-md:ml-2.5">
+                          <img
+                            loading="lazy"
+                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/4406f67a9559310af2ac74b07af2d0fcd6e1ae972e52b0275fc8f5add86b4fc3?apiKey=4c4222db693a45b68fa169800b04b85b&"
+                            alt="Affiliate Program Icon"
+                            className="shrink-0 aspect-square w-[45px]"
+                          />
+                          <h2 className="flex-auto self-start">
+                            Affiliate Program
+                          </h2>
+                        </div>
+                        <div className="flex gap-5 mt-1.5 text-sm leading-6 max-md:flex-wrap">
+                          <img
+                            loading="lazy"
+                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/32adc263287ff7656fe3092b712342c62cf2b4c963041d1a57dc4d98acdacbfc?apiKey=4c4222db693a45b68fa169800b04b85b&"
+                            alt="Affiliate Program Description Icon"
+                            className="shrink-0 my-auto aspect-square w-[45px]"
+                          />
+                          <div className="flex flex-col grow shrink-0 basis-0 w-fit max-md:max-w-full">
+                            <p className="max-md:max-w-full">
+                              Earn by Referring: Simply refer clients to us and
+                              earn a referral commission for every successful
+                              transaction. Plus, enjoy the benefits of recurring
+                              revenue from ongoing contracts.
+                              <br />
+                              Hassle-Free Partnership: We handle all service
+                              delivery and customer support. Your job is to
+                              direct potential clients to us, and we take care
+                              of the rest.
+                              <br />
+                            </p>
+                            <h3 className="mt-4 text-lg font-bold leading-7 text-black max-md:max-w-full">
+                              Reseller Program
+                            </h3>
+                            <p className="mt-5 max-md:max-w-full">
+                              Become a Reseller: Take an active role in
+                              reselling our services. You manage the sales
+                              process, and we support you as needed. From
+                              service delivery to customer support and billing,
+                              we've got it covered. Attractive Earnings: Earn
+                              substantial commissions and benefit from
+                              continuous recurring revenue.
+                              <br />
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex gap-5 self-start mt-7 ml-6 font-bold text-black max-md:ml-2.5">
+                          <img
+                            loading="lazy"
+                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/237914d881b1af1fa1f22498b1dfbcf2de94b06e9aade188cc19a9c7cb3f4e76?apiKey=4c4222db693a45b68fa169800b04b85b&"
+                            alt="White Label Partnership Icon"
+                            className="shrink-0 w-9 aspect-square"
+                          />
+                          <h2 className="flex-auto self-start">
+                            White Label Partnership
+                          </h2>
+                        </div>
+                        <p className="text-sm leading-6 max-md:max-w-full">
+                          Your Brand, Our Expertise: As a White Label Partner,
+                          you can offer our services under your brand name. Gain
+                          access to exclusive trade rates and expand your
+                          portfolio with our cutting-edge technology solutions.
+                          <br />
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex flex-col ml-5 w-[17%] max-md:ml-0 max-md:w-full">
+                      <div className="flex flex-col self-stretch my-auto text-xs font-bold leading-7 text-center text-black max-md:mt-10">
+                        <button className="justify-center px-8 py-2.5 border border-black border-solid rounded-[50px] max-md:px-5">
+                          Learn more
+                        </button>
+                        <button className="justify-center px-8 py-2.5 mt-28 border border-black border-solid rounded-[50px] max-md:px-5 max-md:mt-10">
+                          Learn more
+                        </button>
+                        <button className="justify-center px-8 py-2.5 mt-24 border border-black border-solid rounded-[50px] max-md:px-5 max-md:mt-10">
+                          Learn more
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </article>
+            <div className="flex flex-col ml-5 w-[38%] max-md:ml-0 max-md:w-full">
+              <figure className="flex flex-col pb-4 mt-14 w-full bg-sky-500 rounded-[57px] max-md:mt-10 max-md:max-w-full">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/07e61622d29a7b2288dadee102395465db143427fc656cf534dc36ca4e65b5b8?apiKey=4c4222db693a45b68fa169800b04b85b&"
+                  alt="Partnership Portal Image"
+                  className="w-full border border-black border-solid shadow-sm aspect-[0.79] max-md:max-w-full"
+                />
+              </figure>
+            </div>
+          </div>
+        </main>
+      </section>
+
+      <section className="flex flex-col items-center px-4 py-8 sm:py-24 w-full text-center text-black bg-zinc-100 max-md:px-5 max-md:max-w-full">
+        <h2 className="text-2xl sm:text-5xl tracking-tight capitalize leading-[28px] sm:leading-[61px] max-md:max-w-full container mx-auto">
+          Partner with Us Today! Pick the Partner option that works for you!
+        </h2>
+
+        <p className="mt-5 mb-2 text-xs sm:text-xl leading-4 sm:leading-7 max-w-[1080px] container mx-auto">
+          Leverage our expertise, resources, and reputation to accelerate your
+          growth in the technology space. Join us at CodeAutomation and be part
+          of a future-driven community of successful partners.
+        </p>
+      </section>
+
+      <section>
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+          <figure className="flex flex-col w-[45%] max-md:ml-0 max-md:w-full">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/597aa75dcff64c3d71cdfa2af04c094365125382494f7d86a182efbe5992a175?apiKey=4c4222db693a45b68fa169800b04b85b&"
+              alt="Reseller partner image"
+              className="grow w-full aspect-[0.99] max-md:mt-10 max-md:max-w-full"
+            />
+          </figure>
+          <div className="flex flex-col ml-5 w-[55%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col self-stretch px-5 my-auto max-md:mt-10 max-md:max-w-full">
+              <h2 className="text-5xl text-black leading-[58px] max-md:max-w-full max-md:text-4xl">
+                Reseller Partner
+              </h2>
+              <ul className="mt-14 text-2xl leading-[50px] text-slate-600 max-md:mt-10 max-md:max-w-full">
+                <li>25% commission first year</li>
+                <li>
+                  10% second year providing new clients project revenue grows by
+                  15% per year
+                </li>
+                <li>
+                  We handle billing and first line support and clients day to
+                  day needs
+                </li>
+                <li>We handle invoicing</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="max-md:pl-5">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+          <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+            <article className="flex flex-col grow mt-9 max-md:max-w-full">
+              <h2 className="text-5xl text-black leading-[58px] max-md:max-w-full max-md:text-4xl">
+                Affiliate Partner
+              </h2>
+              <p className="mt-12 text-2xl leading-[50px] text-slate-600 max-md:mt-10 max-md:max-w-full">
+                12% of revenue in first year <br />
+                3% of revenue in second and third year - if new revenue
+                conditions at 5% growth this revenue continues <br />
+                We handle billing and first line support and clients day to day
+                needs and handle invoicing12% of revenue in first year <br />
+                3% of revenue in second and third year - if new revenue
+                conditions at 5% growth this revenue continues <br />
+                We handle billing and first line support and clients day to day
+                needs and handle invoicing
+              </p>
+            </article>
+          </div>
+          <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/3f58c321da0073b73d8f07f88a003e259e2eada55d4e1886e2c1bc94afb30947?apiKey=4c4222db693a45b68fa169800b04b85b&"
+              alt="Affiliate partner related image"
+              className="w-full aspect-square max-md:max-w-full"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="flex gap-5 max-md:flex-col max-md:gap-0">
+        <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+          <img
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/5a5a5da95986662f70077a396b772c58b217a48d72da116b4f016e3b4caabd0b?apiKey=4c4222db693a45b68fa169800b04b85b&"
+            alt=""
+            className="w-full aspect-square max-md:max-w-full"
+            loading="lazy"
+          />
+        </div>
+        <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col grow px-5 mt-16 max-md:mt-10 max-md:max-w-full">
+            <h2 className="text-5xl text-black leading-[58px] max-md:max-w-full max-md:text-4xl max-md:leading-[52px]">
+              White Label Reseller Partner
+            </h2>
+            <div className="flex flex-col mt-12 text-2xl leading-[50px] text-slate-600 max-md:mt-10 max-md:max-w-full">
+              <p className="z-10 max-md:max-w-full">
+                Trade rates <br />
+                20% discount under $250k maintained annual project value <br />
+                25% discount $250k to $500k maintained annual project value{" "}
+                <br />
+                30% over $500k maintained annual project value <br />
+                You resell at what ever rate you like <br />
+                You handle billing and first line support
+              </p>
+              <div
+                className="shrink-0 self-end mt-0 max-w-full rounded-full border-solid border-[41px] border-yellow-400 border-opacity-40 h-[680px] w-[447px] max-md:mt-0"
+                role="presentation"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex justify-center items-center lg:px-10 md:px-5 lg:py-16 py-16 w-full text-black bg-[#F3F3F3] leading-[100%] max-md:px-5 max-md:max-w-full">
+        <div className="flex flex-col w-full max-w-[1582px] max-md:my-10 max-md:max-w-full">
+          <div className="self-center xl:text-4xl lg:text-3xl text-3xl font-medium max-md:max-w-full">
+            Our Services
+          </div>
+
+          <div className="grid xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-5 xl:mt-12 text-xl whitespace-nowrap max-md:flex-wrap mt-10 max-md:max-w-full">
+            <div>
+              <div className="text-center py-4 px-6 bg-white rounded-lg shadow-md max-md:p-3">
+                <p>Anything web</p>
+                <p>CMS, eCommerce, Headless</p>
+              </div>
+              <div className="text-center py-4 px-6 bg-white rounded-lg shadow-md max-md:p-3">
+                <p>Web and Mobile</p>
+                <p>App Development</p>
+              </div>
+            </div>
+            <div className="text-center py-4 px-6 bg-white rounded-lg shadow-md max-md:p-3">
+              App Development
+            </div>
+            <div className="text-center py-4 px-6 bg-white rounded-lg shadow-md max-md:p-3">
+              QA Automation Testing
+            </div>
+            <div className="text-center py-4 px-6 bg-white rounded-lg shadow-md max-md:p-3">
+              AI/ML Services
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
