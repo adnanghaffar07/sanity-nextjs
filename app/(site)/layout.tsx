efd548ed7ba7079a70d189a585bde4454c693237
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2024-05-15 18:42:17 +0500

    clarity setup

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import Head from "next/head"; // Import Head
 import "../globals.css";
 import HomeNavigationContainer from "./components/home-navigation-container";
 import FooterContainer from "./components/footer-container";
 import ProjectDiscussionContainer from "./components/project-discussion-form";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Code Automation - Custom Software and Mobile Development Company in USA",
@@ -17,16 +15,29 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // get all og our pages
   return (
     <html lang="en">
       <body>
+        <Head>
+          {/* Add Clarity tracking code here */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "mc90zj5o4h");
+              `,
+            }}
+          />
+        </Head>
         <div className="flex flex-col bg-white relative">
           <HomeNavigationContainer />
           {children}
           <div className="lg:max-w[506px] ml-auto mr-auto -mb-32 relative z-10 lg:px-0 px-5">
             <ProjectDiscussionContainer />
-          </div>          
+          </div>
           <FooterContainer />
         </div>
       </body>
