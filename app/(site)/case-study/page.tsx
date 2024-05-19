ef172b7d84016707420c79398f73045ef6128e32
umaid@codeautomation.ai <umaid@codeautomation.ai>
2024-05-19 13:13:47 +0500

    Case Study Main Page Changes

--- app/(site)/case-study/page.tsx
+++ app/(site)/case-study/page.tsx
@@ -10,7 +10,9 @@ const Page = () => {
   const [initialCards, SetInitialCards] = useState<any[]>([""]);
   const [isFiltered, SetIsFiltered] = useState<boolean>(false);
   const [originalCards, SetOriginalCards] = useState<any[]>([""]);
-  const [showSelectedFilters, SetSelectedFilters] = useState<any[]>([""]);
+  const [selectedFilters, SetSelectedFilters] = useState<any[]>([]);
+  const [isChecked, SetIsChecked] = useState<boolean>(false);
+  const [checkedCount, SetCheckedCount] = useState<number>(0);
 
   useEffect(() => {
     async function getData() {
@@ -33,16 +35,25 @@ const Page = () => {
         (item: any) => item.title == "CaseStudyInfo"
       );
       const initalArray = caseStudyData[0].cardItemsList;
-      console.log(initalArray);
+      console.log("Initial Array", initalArray);
       SetInitialCards(initalArray);
       SetOriginalCards(initalArray);
-      //SetSelectedFilters(data[0].cardItemsList[0]);
+      
     };
     dataFun();
   }, []);
 
-  function DisplaySelectedFilters() {
-    SetInitialCards(showSelectedFilters);
+  function DisplaySelectedFilters()
+  {
+    console.log("SELECTED FILTERS LENGTH",selectedFilters.length);
+    console.log('SELECTED FILTERS DATA',selectedFilters) 
+    
+    // if(selectedFilters.length != 0)
+    // {
+    //   //SetInitialCards(selectedFilters[0])
+    // }
+    
+  
   }
 
   function ResetFilters() {
@@ -50,25 +61,30 @@ const Page = () => {
       .querySelectorAll("input")
       .forEach((item) => (item.checked = false));
     SetInitialCards(originalCards);
+    SetSelectedFilters([]);
   }
 
-  function GetFilteredData(option: any) {
-    if (!isFiltered) {
+  function GetFilteredData(option: any, event: any) {
+    console.log(event.target.checked);
+
+    if (event.target.checked) {
       // If the checkbox is checked, show the filtered Cards Information
       const techGroup = originalCards.filter(
-        (item: any) => item.group === option
+        (item: any, index: any) => item.group === option && item.group !== null
       );
-      SetInitialCards(techGroup);
-      //SetSelectedFilters([...techGroup])
-      SetIsFiltered(true);
-    } else {
-      //Show Original Cards Information
-      SetInitialCards(originalCards);
 
-      SetIsFiltered(false);
-    }
+       console.log("Tech Group", techGroup);
 
-    console.log("Selected Filters Info:", showSelectedFilters);
+       SetSelectedFilters([...selectedFilters, techGroup]);
+      
+    } 
+
+    else {
+    
+      //Remove the unselect element from the list
+      }
+
+    console.log("Selected Filters Info:", selectedFilters);
   }
 
   return (
@@ -96,26 +112,37 @@ const Page = () => {
       <div className=" flex md:flex-row  justify-center mt-10 mb-10">
         {/* Filter Section    */}
 
-        {/* <section className="md:w-auto sm:w-auto  text-white mx-auto  bg-sky-500 max-w-[480px] min-h-[1301px]">
+        <section className="md:w-auto sm:w-auto  text-white mx-auto  bg-sky-500 max-w-[480px] min-h-[1301px]">
           <h1>Filter Case Studies</h1>
-
           <div className=" flex flex-col gap-40">
             <div className="mt-20 ml-2">
               <h1>Technologies</h1>
               <br></br>
               <ul>
                 <li>
-                 <input type="checkbox" onChange={()=>GetFilteredData('app')} /> 
+                  <input
+                    type="checkbox"
+                    id="app"
+                    onChange={(event) => GetFilteredData("app", event)}
+                  />
                   <span>App Development</span>
                 </li>
 
                 <li>
-                  <input type="checkbox"  onChange={()=>GetFilteredData('web')}  />
+                  <input
+                    type="checkbox"
+                    id="web"
+                    onChange={(event) => GetFilteredData("web", event)}
+                  />
                   <span> Web Development</span>
                 </li>
 
                 <li>
-                  <input type="checkbox"  onClick={()=>GetFilteredData('qa')} />
+                  <input
+                    type="checkbox"
+                    id="qa"
+                    onClick={(event) => GetFilteredData("qa", event)}
+                  />
                   <span> QA Testing</span>
                 </li>
 
@@ -178,165 +205,30 @@ const Page = () => {
             </div>
 
             <div className="flex flex-row items-center ml-1 gap-2">
-             
-            <button  onClick={()=>ResetFilters()} type="button"  className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                  Reset
-                  </button>    
-                  <button type="button" onClick={()=>DisplaySelectedFilters()} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Apply</button>
-
-              
+              <button
+                onClick={() => ResetFilters()}
+                type="button"
+                className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Reset
+              </button>
+              <button
+                type="button"
+                onClick={() => DisplaySelectedFilters()}
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Apply
+              </button>
             </div>
           </div>
-        </section> */}
+        </section>
 
         {/*  Case Study Grid Section    */}
 
-        {/* <section className=" mx-auto">
-          <div className=" md:grid md:grid-cols-2 sm:grid sm:grid-col-1 gap-2 ">
-            <div className="relative">
-              <Image
-                width={404}
-                height={268}
-                className="w-full aspect-[1.52] md:max-w-[304px] sm:max-w-[204px] sm:mb-2"
-                src="/HouseArrest.png"
-                alt="card"
-              ></Image>
-
-              <div className="bg-red-500 py-2 text-center  md:absolute md:bottom-[6rem] md:right-0 sm:absolute sm:bottom-[6rem] sm:right-[6rem]  rounded-[32px_22px_26px_18px]">
-                <img
-                  loading="lazy"
-                  srcSet="PublicInputLogo.png"
-                  className="w-full aspect-[3.13]"
-                />
-              </div>
-
-              <div className="text-base font-light tracking-wide leading-6 max-w-[317px] text-sky-950">
-                PublicInput helps reduce your burdens by helping you to get
-                public engagement and communicate with your audience.
-              </div>
-            </div>
-
-            <div className="relative">
-              <Image
-                width={404}
-                height={268}
-                className="w-full aspect-[1.52] md:max-w-[304px] sm:max-w-[204px] sm:mb-2"
-                src="/PublicInput.png"
-                alt="card"
-              ></Image>
-
-              <div className="bg-yellow-300 py-2 text-center md:absolute md:bottom-[6rem] md:right-0 sm:absolute sm:bottom-[6rem] sm:right-[6rem]  rounded-[32px_22px_26px_18px]">
-                <img
-                  loading="lazy"
-                  srcSet="PublicInputLogo.png"
-                  className="w-full aspect-[3.13]"
-                />
-              </div>
-
-              <div className="text-base font-light tracking-wide leading-6 max-w-[317px] text-sky-950">
-                PublicInput helps reduce your burdens by helping you to get
-                public engagement and communicate with your audience.
-              </div>
-            </div>
-
-            <div className="relative">
-              <Image
-                width={404}
-                height={268}
-                className="w-full aspect-[1.52] md:max-w-[304px] sm:max-w-[204px] sm:mb-2"
-                src="/VestedFinance.png"
-                alt="card"
-              ></Image>
-
-              <div className="bg-yellow-300 py-2 text-center md:absolute md:bottom-[6rem] md:right-0 sm:absolute sm:bottom-[6rem] sm:right-[6rem]  rounded-[32px_22px_26px_18px]">
-                <img
-                  loading="lazy"
-                  srcSet="PublicInputLogo.png"
-                  className="w-full aspect-[3.13]"
-                />
-              </div>
-
-              <div className="text-base font-light tracking-wide leading-6 max-w-[317px] text-sky-950">
-                Vested finance is a fast, secure, and tailored platform that
-                gives you option to invest in either full or fractional shares.
-              </div>
-            </div>
-
-            <div className="relative">
-              <Image
-                width={404}
-                height={268}
-                className="w-full aspect-[1.52] md:max-w-[304px] sm:max-w-[204px] sm:mb-2"
-                src="/InnRoad.png"
-                alt="card"
-              ></Image>
-
-              <div className="py-2 text-center text-white md:absolute md:bottom-[6rem] md:right-0 sm:absolute sm:bottom-[6rem] sm:right-[6rem] bg-lime-500 rounded-[32px_22px_26px_18px]">
-                innRoad
-              </div>
-
-              <div className="text-base font-light  leading-6 max-w-[317px] text-sky-950">
-                InnRoad is a hotel management solution to manage your business
-                by providing various options including online booking, property
-                management and lots more.
-              </div>
-            </div>
-
-            <div className="relative">
-              <Image
-                width={404}
-                height={268}
-                className="w-full aspect-[1.52] md:max-w-[304px] sm:max-w-[204px] sm:mb-2"
-                src="/Dandy.png"
-                alt="card"
-              ></Image>
-
-              <div className="bg-orange-400 py-2 text-center  md:absolute md:bottom-[6rem] md:right-0 sm:absolute sm:bottom-[6rem] sm:right-[6rem]  rounded-[32px_22px_26px_18px]">
-                <img
-                  loading="lazy"
-                  srcSet="GetDandyLogo.png"
-                  className="w-full aspect-[3.13]"
-                />
-              </div>
-
-              <div className="text-base font-light  tracking-wide leading-6 max-w-[317px] text-sky-950">
-                Dandy is an AI and ML-based software that provides options to
-                remove bad reviews and replies to the reviews
-              </div>
-            </div>
-
-            <div className="relative">
-              <Image
-                width={404}
-                height={268}
-                className="w-full aspect-[1.52] md:max-w-[304px] sm:max-w-[204px] sm:mb-2"
-                src="/Tracely.png"
-                alt="card"
-              ></Image>
-
-              <div className="bg-blue-200 py-2 text-center  md:absolute md:bottom-[6rem] md:right-0 sm:absolute sm:bottom-[6rem] sm:right-[6rem]  rounded-[32px_22px_26px_18px]">
-                <img
-                  loading="lazy"
-                  srcSet="GetDandyLogo.png"
-                  className="w-full aspect-[3.13]"
-                />
-              </div>
-
-              <div className="text-base font-light tracking-wide leading-6 max-w-[317px] text-sky-950">
-                Tracely is a location tracking app designed to help people keep
-                track of their family and friends, chat with them or enjoy their
-                newsfeed updates.
-              </div>
-            </div>
-          </div>
-        </section> */}
-
         <section className=" mx-auto">
           <div className=" md:grid md:grid-cols-2 md: gap-10 sm:grid sm:grid-col-1  ">
-            {initialCards.map((item: any, index: any) => { 
-                     
+            {initialCards.map((item: any, index: any) => {
               return (
-               
                 <div key={index}>
                   {item?.cardImage && (
                     <Link href="/career">
