dd18fa9ed3ed5c9a13538ce5ba44e03070693c79
umaid@codeautomation.ai <umaid@codeautomation.ai>
2024-05-25 02:26:18 +0500

    Update page.tsx

--- app/(site)/case-study/page.tsx
+++ app/(site)/case-study/page.tsx
@@ -100,26 +100,28 @@ const Page = () => {
   return (
     <>
       <section className="relative">
-      <div className=" w-full h-[380px] sm:h-[700px] opacity-65 absolute z-[1]"></div>
-      <div className="w-full h-[380px] sm:h-[700px] relative z-0">
-        <Image
-          src='/CaseStudyHero.jpg'
-          alt='CaseStudyHero'
-          loading="lazy"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
+        <div className=" w-full h-[380px] sm:h-[700px] opacity-65 absolute z-[1]"></div>
+        <div className="w-full h-[380px] sm:h-[700px] relative z-0">
+          <Image
+            src="/CaseStudyHero.jpg"
+            alt="CaseStudyHero"
+            loading="lazy"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
 
-      <div className="mt-[130px] sm:mt-[327px] items-center absolute inset-0 flex flex-col z-[2]">
-        <h1 className="text-xl sm:text-5xl font-bold tracking-tight capitalize leading-[48px] text-white text-center">
-        Case Studies
-        </h1>
-        <p className="mt-1 sm:mt-2 mb-2 sm:mb-44 text-xs sm:text-xl font-light tracking-wide leading-4 sm:leading-7  text-white max-w-[280px] sm:max-w-[1080px] xl:px-0 text-center">
-        Explore our case study on code automation, showcasing how innovative strategies significantly enhance efficiency, reduce costs, and accelerate development in software projects.
-        </p>
-      </div>
-    </section>
+        <div className="mt-[130px] sm:mt-[327px] items-center absolute inset-0 flex flex-col z-[2]">
+          <h1 className="text-xl sm:text-5xl font-bold tracking-tight capitalize leading-[48px] text-white text-center">
+            Case Studies
+          </h1>
+          <p className="mt-1 sm:mt-2 mb-2 sm:mb-44 text-xs sm:text-xl font-light tracking-wide leading-4 sm:leading-7  text-white max-w-[280px] sm:max-w-[1080px] xl:px-0 text-center">
+            Explore our case study on code automation, showcasing how innovative
+            strategies significantly enhance efficiency, reduce costs, and
+            accelerate development in software projects.
+          </p>
+        </div>
+      </section>
 
       <section className=" h-full flex flex-col  justify-center gap-4 bg-[#F3F3F3]">
         <div className="self-stretch text-5xl mt-20 text-center text-black capitalize leading-[60px] max-md:text-4xl max-md:leading-[51px]">
