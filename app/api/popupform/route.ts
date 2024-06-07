e8de8da2c70531e5ce45a54564101bf07f0d5165
umaid-ca <umaid@codeautomation.ai>
2024-06-07 18:22:47 +0500

    updated email sending list for checking brevo functionality

--- app/api/popupform/route.ts
+++ app/api/popupform/route.ts
@@ -109,9 +109,13 @@ export async function POST(request: any): Promise<any> {
 
   const mailOptions = {
     from: "umaid@codeautomation.dev",
-    to: ["umaid@codeautomation.dev"],
+    // to: ["umaid@codeautomation.dev"],
     // to: ["umaid@codeautomation.ai"],
-    // to: ["adnan@codeautomation.dev", "jason@codeautomation.dev"],
+    to: [
+      "adnan@codeautomation.dev",
+      "jason@codeautomation.dev",
+      "umaid@codeautomation.dev",
+    ],
     subject: `CA Website Greeting Popup form - ${pageName} page`,
     html: `
     <!DOCTYPE html>
