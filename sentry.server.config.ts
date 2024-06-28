bc78efdf3c9dd9614faf36ae33f77ac438cb919e
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2024-06-28 15:26:52 +0500

    Sentry Integrated

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2dcb69323c58b81e56b37bff044f5f07@o4507508859338752.ingest.us.sentry.io/4507508867923968",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
