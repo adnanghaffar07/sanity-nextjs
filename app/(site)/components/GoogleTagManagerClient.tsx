a0b7404ec8d1c83c4a7329c5401d71389d3de3b5
umaid-ca <umaid@codeautomation.ai>
2024-06-29 00:16:04 +0500

    used script.async = true

--- app/(site)/components/GoogleTagManagerClient.tsx
+++ app/(site)/components/GoogleTagManagerClient.tsx
@@ -1,29 +1,10 @@
-// "use client";
-// import { useEffect } from "react";
-
-// const GoogleTagManagerClient = ({ gtmId }: { gtmId: string }) => {
-//   useEffect(() => {
-//     const script = document.createElement("script");
-//     script.async = true;
-//     script.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
-//     document.head.appendChild(script);
-
-//     return () => {
-//       document.head.removeChild(script);
-//     };
-//   }, [gtmId]);
-
-//   return null;
-// };
-
-// export default GoogleTagManagerClient;
 "use client";
 import { useEffect } from "react";
 
 const GoogleTagManagerClient = ({ gtmId }: { gtmId: string }) => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.defer = true; // Use async to load the script in the background
+    script.async = true;
     script.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
     document.head.appendChild(script);
 
