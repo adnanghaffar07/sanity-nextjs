583da514376a42992f75513818baf780a8438af6
umaid-ca <umaid@codeautomation.ai>
2024-07-01 18:44:43 +0500

    added GTM script provided by GTM website

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import dynamic from "next/dynamic";
 import React, { Suspense } from "react";
 // import { Inter } from "next/font/google";
 import "../globals.css";
 import HomeNavigationContainer from "./components/home-navigation-container";
-import { GoogleTagManager } from "@next/third-parties/google";
+// import { GoogleTagManager } from "@next/third-parties/google";
 // const GoogleTagManagerClient = dynamic(
 //   () => import("./components/GoogleTagManagerClient"),
 //   {
@@ -39,8 +40,29 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-MJG35754" />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MJG35754');
+            `,
+          }}
+        />
+      </Head>
+      {/* <GoogleTagManager gtmId="GTM-MJG35754" /> */}
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MJG35754"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <div className="flex flex-col bg-white relative">
           <HomeNavigationContainer />
           {children}
