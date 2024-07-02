4c318a36d5b08fd4e2bb39c361f12c2c3a04ccfe
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2024-07-02 18:03:06 +0500

    diamonds added

--- app/(site)/case-studies/[singlecase]/page.tsx
+++ app/(site)/case-studies/[singlecase]/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { client } from "../../../../sanity/lib/client";
 import HouseArrestBanner from "../../components/HouseArrestBanners";
+import ScrollAnimation from "../../components/ScrollAnimation";
+
 
 
 import { urlForImage } from "@/sanity/lib/image";
@@ -61,10 +63,10 @@ const page = async ({ params }: { params: { singlecase: string } }) => {
         loading="lazy"
         src="/Subtract-left.png"
         alt="eclipse icon"
-        className="absolute w-[270px] z-0"
+        className="absolute w-[180px] z-0"
       />
       {/* Introduction Section */}
-      
+      <ScrollAnimation>
       <div className="flex flex-col xl:flex-row gap-10 xl:gap-36  mt-0 md:mt-10 px-6 md:px-16 max-w-7xl mx-auto">
         <div>
           <h2 className="text-2xl font-semibold mb-4">
@@ -88,105 +90,119 @@ const page = async ({ params }: { params: { singlecase: string } }) => {
           ))}
         </div>
       </div>
-      <img
-        loading="lazy"
-        src="/Subtract-right.png"
-        alt="eclipse icon"
-        className="absolute right-0 w-[270px] z-0"
-      />
-
-      {/* Tools and Technologies Used Section */}
-    
-      <section className="px-6 md:px-16 py-10 md:py-16 max-w-7xl mx-auto ">
+      </ScrollAnimation>
       <img
         loading="lazy"
         src="/vector1.png"
         alt="eclipse icon"
-        className="absolute w-[70px] z-0"
+        className="absolute w-[50px] md:w-[90px] z-0"
       />
-   
-        <div className="border border-[#0a8ffc] shadow-md p-6 rounded-lg">
-          <h3 className="text-2xl font-semibold text-center my-4 md:my-8 font-klein">
-            {data?.toolsandtechusedtitle}
-          </h3>
 
-          <p className="text-lg font-light text-center leading-9 text-black">
-            {data?.toolsandtechdescription}
-          </p>
+      {/* Tools and Technologies Used Section */}
+      <ScrollAnimation>
+      <section className="px-6 md:px-16 py-10 md:py-16 max-w-7xl mx-auto ">
+        <div></div>
+        <h3 className="text-2xl font-semibold text-center my-4 md:my-8 font-klein">
+          {data?.toolsandtechusedtitle}
+        </h3>
 
-          <h3 className="text-2xl font-semibold text-center my-4 md:my-8 font-klein">
-            {data?.toolsandtechusedheading}
-          </h3>
+        <p className="text-lg font-light text-center leading-9 text-black">
+          {data?.toolsandtechdescription}
+        </p>
+        <img
+          loading="lazy"
+          src="/vector2.png"
+          alt="eclipse icon"
+          className="absolute right-0 w-[30px] md:w-[90px] z-0"
+        />
+        <h3 className="text-2xl font-semibold text-center my-4 md:my-8 font-klein">
+          {data?.toolsandtechusedheading}
+        </h3>
 
-          {data.caseStudiesToolsSection ? (
-            <div className="container mt-10">
-              <div className="flex flex-col sm:flex sm:flex-row sm:flex-wrap justify-center gap-y-5 md:gap-y-10">
-                {data.caseStudiesToolsSection.toolsTech.map(
-                  (tool: any, toolIndex: any) => (
-                    <div
-                      key={toolIndex}
-                      className="w-full m-4 sm:m-0 sm:w-1/2 lg:w-1/3"
-                    >
-                      <div className="flex md:justify-center gap-2">
-                        <div className="flex flex-row gap-3">
-                          {tool.images?.map((logoRef: any, logoIndex: any) => {
-                            const logoData = dataLogo.find(
-                              (logo: any) => logo._id === logoRef._ref
+        {data.caseStudiesToolsSection ? (
+          <div className="container mt-10">
+            <div className="flex flex-col sm:flex sm:flex-row sm:flex-wrap justify-center gap-y-5 md:gap-y-10">
+              {data.caseStudiesToolsSection.toolsTech.map(
+                (tool: any, toolIndex: any) => (
+                  <div
+                    key={toolIndex}
+                    className="w-full m-4 sm:m-0 sm:w-1/2 lg:w-1/3"
+                  >
+                    <div className="flex md:justify-center gap-2">
+                      <div className="flex flex-row gap-3">
+                        {tool.images?.map((logoRef: any, logoIndex: any) => {
+                          const logoData = dataLogo.find(
+                            (logo: any) => logo._id === logoRef._ref
+                          );
+                          if (logoData) {
+                            return (
+                              <div key={logoIndex}>
+                                <img
+                                  src={urlForImage(logoData.image).toString()}
+                                  alt={logoData.heading}
+                                  className="h-10 md:h-14 object-contain"
+                                />
+                              </div>
                             );
-                            if (logoData) {
-                              return (
-                                <div key={logoIndex}>
-                                  <img
-                                    src={urlForImage(logoData.image).toString()}
-                                    alt={logoData.heading}
-                                    className="h-10 md:h-14 object-contain"
-                                  />
-                                </div>
-                              );
-                            } else {
-                              return null;
-                            }
-                          })}
-                        </div>
+                          } else {
+                            return null;
+                          }
+                        })}
+                      </div>
 
-                        <div className="flex flex-col w-auto my-auto">
-                          <h3 className="text-xl md:text-2xl tracking-wider font-bold font-klein">
-                            {tool.heading}
-                          </h3>
-                          <p className="mt-1 text-lg tracking-wide">
-                            {tool.detail}
-                          </p>
-                        </div>
+                      <div className="flex flex-col w-auto my-auto">
+                        <h3 className="text-xl md:text-2xl tracking-wider font-bold font-klein">
+                          {tool.heading}
+                        </h3>
+                        <p className="mt-1 text-lg tracking-wide">
+                          {tool.detail}
+                        </p>
                       </div>
                     </div>
-                  )
-                )}
-              </div>
+                  </div>
+                )
+              )}
             </div>
-          ) : (
-            <ul>
-              {data.technologiesused?.map((tech: any) => (
-                <li
-                  className="text-xs md:text-xl leading-4 md:leading-8 font-light"
-                  key={tech._key}
-                >
-                  <span className="text-lg font-bold">
-                    {tech.heading}:{" "}
-                  </span>
-                  <span className="text-lg font-light">
-                    {tech.description}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      
+          </div>
+        ) : (
+          <ul>
+            {data.technologiesused?.map((tech: any) => (
+              <li
+                className="text-xs md:text-xl leading-4 md:leading-8 font-light"
+                key={tech._key}
+              >
+                <span className="text-lg font-bold">
+                  {tech.heading}:{" "}
+                </span>
+                <span className="text-lg font-light">
+                  {tech.description}
+                </span>
+              </li>
+            ))}
+          </ul>
+        )}
+
+
       </section>
+      </ScrollAnimation>
+      <img
+        loading="lazy"
+        src="/vector1.png"
+        alt="eclipse icon"
+        className="absolute w-[30px] md:w-[90px] z-0"
+      />
+      <img
+        loading="lazy"
+        src="/vector3.png"
+        alt="eclipse icon"
+        className="absolute right-0 w-[80px] md:w-[160px] z-0"
+      />
 
       {/*  Application Features  */}
-      <section className="px-6 md:px-16 py-5 md:py-16 max-w-7xl mx-auto">
-        <div className="border border-[#0a8ffc] shadow-md p-6 rounded-lg">
+      <ScrollAnimation>
+      <section className="">
+
+        <div className="px-6 md:px-16 py-5 md:py-16 max-w-7xl mx-auto">
           {data.features?.featureslist.map((feature: any, index: number) => (
             <div key={feature._key} className="flex flex-col items-center">
               {/* Render heading */}
@@ -236,113 +252,155 @@ const page = async ({ params }: { params: { singlecase: string } }) => {
             </div>
           ))}
         </div>
+
+        {/* <img
+          loading="lazy"
+          src="/vector4.png"
+          alt="eclipse icon"
+          className="absolute w-[170px] z-0 "
+        /> */}
       </section>
+      </ScrollAnimation>
+      <img
+        loading="lazy"
+        src="/vector3.png"
+        alt="eclipse icon"
+        className="absolute right-0 w-[50px] md:w-[160px] z-0"
+      />
 
 
 
 
-      <div className="px-6 md:px-16 max-w-7xl mx-auto">
-        {data?.secondaryimage?.asset && (
-          <img
-            loading="lazy"
-            src={urlForImage(data.secondaryimage.asset)}
-            alt={data.secondaryimage.alt}
-            className="object-cover"
-          />
-        )}
-        {/* Client Background & We Provided Section */}
-        {/* {data.projectoverviewtitle && (
+      <section >
+
+        <div>
+        <ScrollAnimation>
+          <div className="px-6 md:px-16 py-5 md:py-16 max-w-7xl mx-auto">
+            {data?.secondaryimage?.asset && (
+              <img
+                loading="lazy"
+                src={urlForImage(data.secondaryimage.asset)}
+                alt={data.secondaryimage.alt}
+                className="object-cover"
+              />
+            )}
+            {/* Client Background & We Provided Section */}
+            {/* {data.projectoverviewtitle && (
           <h3 className="text-2xl font-semibold  my-4 md:my-8">
             {data.projectoverviewtitle}
           </h3>
         )} */}
-
-        {data.projectoverview ? (
-          <div className="">
-            <div className="mb-6 md:mb-10">
-              <h3 className="text-2xl font-semibold mb-6">
-                {data.projectoverview.heading}
-              </h3>
-              <p>{data.projectoverview.detail}</p>
-            </div>
-            <div className="flex flex-wrap justify-center gap-6">
-              {data.projectoverview.projectoverviewdetail.map((project: any) => (
-                <div
-                  key={project._key}
-                  className="rounded-3xl w-full sm:w-[45%] shadow-md text-black p-6 sm:p-8 md:p-10"
-                  style={{
-                    background: `linear-gradient(to right, ${project.leftcolor}, ${project.rightcolor})`,
-                  }}
-                >
-                  <h1 className="text-2xl text-center text-white font-semibold p-2">
-                    {project.heading}
-                  </h1>
-                  <p className="text-white text-center">{project.detail}</p>
-                </div>
-              ))}
-            </div>
           </div>
-        ) : (
-          <div className="hidden"></div>
+          </ScrollAnimation>
+
+          {data?.secondaryimage?.asset && (
+          <img
+            loading="lazy"
+            src="/vector3.png"
+            alt="eclipse icon"
+            className="absolute right-0 w-[100px] md:w-[170px] z-0"
+          /> 
         )}
+          <img
+            loading="lazy"
+            src="/vector1.png"
+            alt="eclipse icon"
+            className="absolute w-[30px] md:w-[65px] z-0"
+          />
+     <ScrollAnimation>
+          <div className="px-6 md:px-16 max-w-7xl mx-auto">
+            {data.projectoverview ? (
+              <div className="">
+                <div className="mb-6 md:mb-10">
+                  <h3 className="text-2xl font-semibold mb-6">
+                    {data.projectoverview.heading}
+                  </h3>
+                  <p>{data.projectoverview.detail}</p>
+                </div>
+                <div className="flex flex-wrap justify-center gap-6">
+                  {data.projectoverview.projectoverviewdetail.map((project: any) => (
+                    <div
+                      key={project._key}
+                      className="rounded-3xl w-full sm:w-[45%] shadow-md text-black p-6 sm:p-8 md:p-10"
+                      style={{
+                        background: `linear-gradient(to right, ${project.leftcolor}, ${project.rightcolor})`,
+                      }}
+                    >
+                      <h1 className="text-2xl text-center text-white font-semibold p-2">
+                        {project.heading}
+                      </h1>
+                      <p className="text-white text-center">{project.detail}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <div className="hidden"></div>
+            )}
 
-        {data.applicationtestingheading ||
-          data.typeoftestingheading ||
-          data.testingimage ? (
-          <div>
-            <div className="flex gap-5 max-md:flex-col max-md:gap-0 py-10 md:py-16">
-              <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col text-2xl gap-10 md:gap-18 text-black max-md:max-w-full">
-                  {/*  Application Testing  */}
-                  <div>
-                    <h2 className="text-2xl font-semibold  mb-4">
-                      {data.applicationtestingheading}
-                    </h2>
-                    <p className="text-lg  font-light leading-8 md:leading-8">
-                      {data.applicationtestingdescription}
-                    </p>
+            {data.applicationtestingheading ||
+              data.typeoftestingheading ||
+              data.testingimage ? (
+              <div>
+                <div className="flex gap-5 max-md:flex-col max-md:gap-0 py-10 md:py-16">
+                  <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+                    <div className="flex flex-col text-2xl gap-10 md:gap-18 text-black max-md:max-w-full">
+                      {/*  Application Testing  */}
+                      <div>
+                        <h2 className="text-2xl font-semibold  mb-4">
+                          {data.applicationtestingheading}
+                        </h2>
+                        <p className="text-lg  font-light leading-8 md:leading-8">
+                          {data.applicationtestingdescription}
+                        </p>
+                      </div>
+                      {/*  Types of Testing  */}
+                      <div>
+                        <h2 className="text-2xl font-semibold  mb-4">
+                          {data.typeoftestingheading}
+                        </h2>
+
+                        <ul className=" mx-10">
+                          {data.typeoftestinglist?.map((testingType: any) => (
+                            <li className=" list-disc" key={testingType._key}>
+                              <span className="text-lg  font-light">
+                                {testingType.value}
+                              </span>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
                   </div>
-                  {/*  Types of Testing  */}
-                  <div>
-                    <h2 className="text-2xl font-semibold  mb-4">
-                      {data.typeoftestingheading}
-                    </h2>
-
-                    <ul className=" mx-10">
-                      {data.typeoftestinglist?.map((testingType: any) => (
-                        <li className=" list-disc" key={testingType._key}>
-                          <span className="text-lg  font-light">
-                            {testingType.value}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
+                  <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+                    {data.testingimage && (
+                      <img
+                        loading="lazy"
+                        src={urlForImage(data.testingimage).toString()}
+                        className="mt-9 w-full aspect-[1.37] max-md:max-w-full"
+                      />
+                    )}
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-                {data.testingimage && (
-                  <img
-                    loading="lazy"
-                    src={urlForImage(data.testingimage).toString()}
-                    className="mt-9 w-full aspect-[1.37] max-md:max-w-full"
-                  />
-                )}
-              </div>
-            </div>
+            ) : (
+              <div className="hidden"></div>
+            )}
           </div>
-        ) : (
-          <div className="hidden"></div>
-        )}
-      </div>
+          </ScrollAnimation>
+        </div>
+      </section>
+
+
       <img
         loading="lazy"
-        src="/Subtract-left.png"
+        src="/vector1.png"
         alt="eclipse icon"
-        className="absolute w-[270px] md:w-[320px] z-0"
-
+        className="absolute w-[30px] md:w-[60px] z-0"
       />
-      <div className="px-6 md:px-16 max-w-7xl mx-auto">
+       <ScrollAnimation>
+      <section> 
+          <div className="px-6 md:px-16 max-w-7xl mx-auto">
         <div className="w-full my-8 md:my-10  mx-auto">
           <h3 className="text-2xl font-semibold  mb-4">
             {data?.projectscopeheading}
@@ -370,14 +428,17 @@ const page = async ({ params }: { params: { singlecase: string } }) => {
           </ul>
         </div>
       </div>
+      </section></ScrollAnimation>
+    
       <img
         loading="lazy"
         src="/Subtract-right.png"
         alt="eclipse icon"
         className="absolute right-0 w-[270px] md:w-[370px] z-0"
       />
-
-      <div className="px-6 md:px-16 pb-10 max-w-7xl mx-auto">
+          <ScrollAnimation>
+<section>
+<div className="px-6 md:px-16 pb-10 max-w-7xl mx-auto">
         <h3 className="text-2xl font-semibold my-4 md:my-8">
           {data?.ourapproachheading}
         </h3>
@@ -426,6 +487,9 @@ const page = async ({ params }: { params: { singlecase: string } }) => {
           {data?.conclusion && data.conclusion}
         </p>
       </div>
+</section>
+</ScrollAnimation>
+      
     </div>
 
   );
