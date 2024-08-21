6a0e64bba76d8ded56565501570c634f0c14cddd
umaid-ca <umaid@codeautomation.ai>
2024-08-21 23:24:56 +0500

    increased the home page slider duration

--- app/(site)/components/HomePageSlider.tsx
+++ app/(site)/components/HomePageSlider.tsx
@@ -10,9 +10,9 @@ const HomePageSlider = () => {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    speed: 2000,
+    speed: 3500,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 7000,
     dots: false,
 
     arrows: false,
@@ -24,41 +24,45 @@ const HomePageSlider = () => {
       <div>
         <div className="lg:text-4xl text-2xl font-bold text-center capitalize max-md:max-w-full">
           <h1 className="title capitalize" style={{ lineHeight: "1" }}>
-          Leading Software Development Company           </h1>
+            Leading Software Development Company{" "}
+          </h1>
         </div>
         <div
           className="lg:text-2xl text-base text-center mt-5 xl:mt-5 max-md:max-w-full"
           style={{ lineHeight: "1.3" }}
         >
-          Transform your Ideas to Reality through Top Software Development Company dedicated to Your Success.
+          Transform your Ideas to Reality through Top Software Development
+          Company dedicated to Your Success.
         </div>
       </div>
 
       <div>
         <div className="lg:text-4xl text-2xl font-bold text-center capitalize max-md:max-w-full">
           <h2 className="title capitalize" style={{ lineHeight: "1" }}>
-          Custom Software Development for Your Needs
+            Custom Software Development for Your Needs
           </h2>
         </div>
         <div
           className="lg:text-2xl text-base text-center mt-5 xl:mt-5 max-md:max-w-full"
           style={{ lineHeight: "1.3" }}
         >
-         Empower your business with tailored digital solutions and innovations from CodeAutomation.ai
+          Empower your business with tailored digital solutions and innovations
+          from CodeAutomation.ai
         </div>
       </div>
 
       <div>
         <div className="lg:text-4xl text-2xl font-bold text-center capitalize max-md:max-w-full">
           <h2 className="title capitalize" style={{ lineHeight: "1" }}>
-          Automate Your QA Testing
+            Automate Your QA Testing
           </h2>
         </div>
         <div
           className="lg:text-2xl text-base text-center mt-5 xl:mt-5 xl:max-w-[1100px] 2xl:max-w-[1200px] xl:mx-auto"
           style={{ lineHeight: "1.3" }}
         >
-          Streamline your software quality with expert QA testing services designed to automate and enhance every step of the process.
+          Streamline your software quality with expert QA testing services
+          designed to automate and enhance every step of the process.
         </div>
       </div>
 
