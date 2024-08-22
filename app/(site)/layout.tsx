847715d5ddb6b67f17f265cfebeb6a3b695ba799
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2024-08-22 18:06:08 +0500

    Update layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,11 +1,10 @@
-
 import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 import React, { Suspense } from "react";
-// import { Inter } from "next/font/google";
+import Head from "next/head";
 import "../globals.css";
 import HomeNavigationContainer from "./components/home-navigation-container";
-// import { GoogleTagManager } from "@next/third-parties/google";
+
 const GoogleTagManagerClient = dynamic(
   () => import("./components/GoogleTagManagerClient"),
   {
@@ -24,7 +23,7 @@ const ScrollToTop = dynamic(() => import("./components/ScrollToTop"), {
 const GreetingPopup = dynamic(() => import("./components/GreetingPopup"), {
   suspense: true,
 });
-// const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title:
     "Software Development Company in the USA | CodeAutomation",
@@ -37,14 +36,113 @@ export const metadata: Metadata = {
   ],
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      {/* <GoogleTagManager gtmId="GTM-MJG35754" /> */}
+      <Head>
+      <link rel="canonical" href="https://codeautomation.xn--ai-02t"/>
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1960013544428790');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1960013544428790&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* JSON-LD Structured Data */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "http://schema.org",
+            "@graph": [
+              {
+                "@type": "Webpage",
+                "Name": "Code Automation",
+                "headline": "Software Development Company in the USA | CodeAutomation",
+                "url": "https://codeautomation.ai/",
+                "@id": "https://codeautomation.ai/",
+                "inLanguage": "en-US",
+                "datePublished": "2024-08-20",
+                "description":
+                  "CodeAutomation.ai is a leading software development company specialized in custom software development services. Schedule a Call & build with experts.",
+                "image": "https://codeautomation.ai/logo.svg",
+                "speakable": { "@type": "SpeakableSpecification" },
+                "potentialAction": {
+                  "@type": "SearchAction",
+                  "target": "Software Development Company USA",
+                },
+                "isPartOf": { "@type": "WebSite", "@id": "https://codeautomation.ai/" },
+                "mainEntity": [{ "@id": "https://codeautomation.ai/" }],
+                "hasPart": { "@id": "https://codeautomation.ai/" },
+              },
+              {
+                "@type": "CreativeWorkSeries",
+                "@id": "https://codeautomation.ai/",
+                "isPartOf": { "@id": "https://codeautomation.ai/" },
+                "name": "Software Development Company USA",
+                "aggregateRating": {
+                  "@type": "AggregateRating",
+                  "bestRating": "5",
+                  "ratingValue": "4.9",
+                  "reviewCount": "100",
+                  "url": "https://maps.app.goo.gl/j7mxdT34Z8Yg1FyU6",
+                },
+              },
+              {
+                "@type": "Service",
+                "@id": "https://codeautomation.ai/",
+                "mainEntityOfPage": { "@id": "https://codeautomation.ai/" },
+                "name": "Software Development Company in the USA | CodeAutomation",
+                "serviceType": ["Software Development Company"],
+                "image": "https://codeautomation.ai/who-we-are.png",
+                "provider": {
+                  "@type": "Organization",
+                  "url": "https://codeautomation.ai/",
+                  "Name": "Code Automation",
+                  "mainEntityOfPage": { "@id": "https://codeautomation.ai/" },
+                },
+                "areaServed": {
+                  "@type": "AdministrativeArea",
+                  "name": "https://maps.app.goo.gl/j7mxdT34Z8Yg1FyU6",
+                },
+                "audience": {
+                  "@type": "Audience",
+                  "name": "Software Development Company USA",
+                },
+              },
+              {
+                "@type": "AggregateRating",
+                "bestRating": "5",
+                "ratingValue": "5",
+                "reviewCount": "9",
+                "url": "https://clutch.co/profile/codeautomationai",
+              },
+            ],
+          })}
+        </script>
+      </Head>
       <body>
         <noscript>
           <iframe
