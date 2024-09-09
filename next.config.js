f39719476167aa595ce8844311f83f97623c1d6e
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2024-09-09 18:00:50 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,6 +16,26 @@ module.exports = {
           destination: '/services/custom-cms-development-services',
           permanent: true,
         },
+        {
+          source: '/services/headless-cms',
+          destination: '/services/headless-cms-development-services',
+          permanent: true,
+        },
+        {
+          source: '/services/design-services',
+          destination: '/services/ui-ux-design-services',
+          permanent: true,
+        },
+        {
+          source: '/services/ai-services',
+          destination: '/services/ai-software-development-services',
+          permanent: true,
+        },
+        {
+          source: '/services/qa-web-testing',
+          destination: '/services/qa-testing-services',
+          permanent: true,
+        },
       ];
     },
   };
