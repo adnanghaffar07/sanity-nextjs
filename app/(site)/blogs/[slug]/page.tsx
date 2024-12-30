e511516a14c4a76ec7309b84b92dfcea9792ea9c
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2024-12-30 22:55:52 +0500

    changes

--- app/(site)/blogs/[slug]/page.tsx
+++ app/(site)/blogs/[slug]/page.tsx
@@ -352,7 +352,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
       }
       {/* Project Overview Section */}
       {data.projectoverviewtitle &&
-        <div className="bg-gray-100 text-black px-6 md:px-16 py-10 md:py-16">
+        <div className="bg-gray-50 text-black px-6 md:px-16 py-10 md:py-16">
           <div className="max-w-7xl mx-auto">
             <h2 className="text-xl font-bold mb-6">
               {data.projectoverviewtitle}
--- app/(site)/components/HomePage-Tech.tsx
+++ app/(site)/components/HomePage-Tech.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 
 // Define the type for the technology categories
@@ -12,7 +13,7 @@ type TechnologyCategory =
 // Define the structure of the technologies data
 const technologiesData: Record<
   TechnologyCategory,
-  { name: string; logo: string }[]
+  { name: string; logo: string; link?: string }[]
 > = {
   "mobile-development": [
     { name: "Flutter", logo: "/mobile3.png" },
@@ -28,11 +29,10 @@ const technologiesData: Record<
     { name: "HTML5", logo: "/html5.png" },
     { name: "Blockchain", logo: "/blockchain.png" },
     { name: "Mobile Angular UI", logo: "/web3.png" },
-
   ],
   "web-development": [
     { name: "Node.js", logo: "/web1.png" },
-    { name: "React.js", logo: "/web2.png" },
+    { name: "React.js", logo: "/web2.png", link: "/services/react-js-development-services" }, // Add a link for React
     { name: "Angular", logo: "/web3.png" },
     { name: "Vue.js", logo: "/web4.png" },
     { name: "Laravel", logo: "/web5.png" },
@@ -46,7 +46,7 @@ const technologiesData: Record<
     { name: "Python", logo: "/python.png" },
     { name: "Swift", logo: "/prog2.png" },
     { name: "Java", logo: "/prog3.png" },
-    { name: "PHP", logo: "/prog4.png" },
+    { name: "PHP", logo: "/prog4.png", link: "/services/php-development-services" }, // Add a link for PHP
     { name: "C#", logo: "/prog5.png" },
     { name: "C++", logo: "/prog6.png" },
     { name: "Ruby", logo: "/prog7.png" },
@@ -81,11 +81,10 @@ const TechnologiesSection = () => {
           {Object.keys(technologiesData).map((category) => (
             <button
               key={category}
-              className={`px-4 py-2 text-sm rounded-lg ${
-                selectedCategory === category
+              className={`px-4 py-2 text-sm rounded-lg ${selectedCategory === category
                   ? "bg-[#1D92FB] text-white"
                   : "bg-[#D9D9D9] text-black"
-              } hover:bg-[#1D92FB] border hover:text-white`}
+                } hover:bg-[#1D92FB] border hover:text-white`}
               onClick={() => handleButtonClick(category as TechnologyCategory)}
             >
               {category.replace(/-/g, " ").toUpperCase()}
@@ -98,8 +97,23 @@ const TechnologiesSection = () => {
               key={index}
               className="flex flex-col items-center text-center w-1/4 sm:w-1/4 md:w-1/4 lg:w-1/6"
             >
-              <img src={tech.logo} alt={tech.name} className="w-16 h-16 mb-2" />
-              <span className="text-sm md:text-md font-md">{tech.name}</span>
+              {tech.link ? (
+                <Link href={tech.link} className="group">
+                  <img
+                    src={tech.logo}
+                    alt={tech.name}
+                    className="w-16 h-16 mb-2 group-hover:opacity-80"
+                  />
+                  <span className="text-sm md:text-md font-md group-hover:underline">
+                    {tech.name}
+                  </span>
+                </Link>
+              ) : (
+                <>
+                  <img src={tech.logo} alt={tech.name} className="w-16 h-16 mb-2" />
+                  <span className="text-sm md:text-md font-md">{tech.name}</span>
+                </>
+              )}
             </div>
           ))}
         </div>
