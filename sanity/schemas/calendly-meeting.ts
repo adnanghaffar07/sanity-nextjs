7dc264f6cd66d1e9c31e993bcfde0774c05eb628
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2025-01-12 16:06:08 +0500

    Update calendly-meeting.ts

--- sanity/schemas/calendly-meeting.ts
+++ sanity/schemas/calendly-meeting.ts
@@ -34,6 +34,11 @@ export default {
         title: "Meeting End",
         type: "datetime",
       },
+      {
+        name: "eventName",
+        title: "Event Name",
+        type: "string",
+      },
     ],
   };
   
