c169ea0c823e4c1b03cf643453a4d197b7f442ed
Adnan Ghaffar <adnanghaffar_308@yahoo.com>
2025-02-26 18:39:00 +0500

    Update page.tsx

--- app/(site)/blogs/[slug]/page.tsx
+++ app/(site)/blogs/[slug]/page.tsx
@@ -121,13 +121,13 @@ const portableTextComponents: Partial<PortableTextReactComponents> = {
   },
   block: {
     h1: ({ children }: { children?: React.ReactNode }) => (
-      <h1 className="text-4xl font-bold my-4">{children}</h1>
+      <h1 className="lg:text-4xl text-2xl font-bold my-4">{children}</h1>
     ),
     h2: ({ children }: { children?: React.ReactNode }) => (
-      <h2 className="text-3xl font-semibold my-3">{children}</h2>
+      <h2 className="lg:text-3xl text-2xl font-semibold my-3">{children}</h2>
     ),
     h3: ({ children }: { children?: React.ReactNode }) => (
-      <h3 className="text-2xl font-semibold">{children}</h3>
+      <h3 className="lg:text-2xl text-xl font-semibold">{children}</h3>
     ),
     h4: ({ children }: { children?: React.ReactNode }) => (
       <h4 className="text-xl font-medium my-2">{children}</h4>
