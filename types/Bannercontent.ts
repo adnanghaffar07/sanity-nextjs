7fbf9be727d2b8f1311d7702070d0244960d5a8a
Adnan Ghaffar <adnanghaffar_308@yahoo.com>
2025-05-22 16:59:47 +0500

    Update Bannercontent.ts

--- types/Bannercontent.ts
+++ types/Bannercontent.ts
@@ -3,7 +3,7 @@ import { PortableTextBlock } from "sanity";
 export type Bannercontent = {
     _id: string;
     _createdAt: Date;
-    title: string;
+    title: string; 
     name: string;
     slug: string;
     content: PortableTextBlock[];
