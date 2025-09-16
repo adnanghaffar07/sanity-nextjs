9c9c88ec7751b80e140ed162f185400831312ccc
Ayesha-CodeAutomation <163011402+ayesha-codeautomation-dev@users.noreply.github.com>
2025-09-16 19:39:32 +0500

    Update page.tsx

--- app/(site)/blogs/[slug]/page.tsx
+++ app/(site)/blogs/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { PortableText, PortableTextReactComponents } from "@portabletext/react";
+import { PortableText, PortableTextMarkComponentProps, PortableTextReactComponents } from "@portabletext/react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { client } from "../../../../sanity/lib/client";
@@ -6,6 +6,7 @@ import { urlForImage } from "@/sanity/lib/image";
 import SocialShare from "../../components/socialShare";
 import BlogsFaq from "../../components/BlogsFaq";
 import AuthorBio from "../../components/AuthorBio";
+import Link from "next/link";
 
 async function getValueData(slug: string) {
   const queryValue = `*[_type == 'portfolio' && slug == '${slug}'][0]`;
@@ -94,8 +95,33 @@ export async function generateMetadata({ params }: { params: { slug: string } })
     },
   };
 }
+// For the Link mark component
+const LinkMark = ({ value, children }: PortableTextMarkComponentProps) => {
+  const target = (value?.href || '').startsWith('http') ? '_blank' : undefined
+  return (
+    <a
+      href={value?.href}
+      target={target}
+      className="text-blue-600 font-light underline"
+      rel={target === '_blank' ? 'noindex nofollow' : ''}
+    >
+      {children}
+    </a>
+  )
+}
 
+// For the InternalLink mark component  
+const InternalLinkMark = ({ value, children }: PortableTextMarkComponentProps) => {
+  return <Link href={`/blogs/${value?.slug}`}>{children}</Link>
+}
 const portableTextComponents: Partial<PortableTextReactComponents> = {
+   marks: {
+    link: LinkMark,
+    internalLink: InternalLinkMark,
+    //  strong: ({ children }: { children?: React.ReactNode }) => (
+    //    <span className="font-bold">{children}</span> // No `<strong>` tag, only class-based styling
+    //  ),
+  },
   types: {
     image: ({ value }: { value: any }) => (
       <img
@@ -183,11 +209,9 @@ const portableTextComponents: Partial<PortableTextReactComponents> = {
     ),
   },
 
-  marks: {
-    strong: ({ children }: { children?: React.ReactNode }) => (
-      <span className="">{children}</span> // No `<strong>` tag, only class-based styling
-    ),
-  },
+  // marks: {
+  //  
+  // },
 
 };
 
